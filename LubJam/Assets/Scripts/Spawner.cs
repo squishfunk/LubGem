95bb6e2f2f246e080734f3a44339136ad340c2ed
Damian <kakaka123321@wp.pl>
2021-01-29 20:48:27 +0100

    Add item spawner

---
+++ LubJam/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnLocations;
+    public GameObject item;
+
+    private Vector3 respawnLocation;
+
+
+	void Awake()
+	{
+        spawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        item = (GameObject)Resources.Load("Item1", typeof(GameObject));
+
+        respawnLocation = item.transform.position;
+
+        SpawnItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnItem()
+	{
+        int spawn = Random.Range(0, spawnLocations.Length);
+        GameObject.Instantiate(item, spawnLocations[spawn].transform.position, Quaternion.identity);
+	}
+}
