3349b9c5e95151834c7ec88eacd208756da4d91b
MadMonk <mmajewski333@gmail.com>
2021-01-30 12:28:21 +0100

    added interaction with item
    my scene added

---
+++ LubJam/Assets/Scripts 1/PickUpScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public Transform PlayerHands;
+
+    public ItemInteraction itemInteraction;
+
+
+    private void Awake()
+    {   
+    }
+
+    private void OnMouseDown()
+    {
+        if (!itemInteraction.CanBePicked) return;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = PlayerHands.position;
+        this.transform.parent = GameObject.Find("PlayerHands").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+}
---
+++ LubJam/Assets/Scripts 1/Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnLocations;
+    public GameObject[] items;
+
+    private Vector3 respawnLocation;
+
+
+	void Awake()
+	{
+        spawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        items = Resources.LoadAll<GameObject>("Items");
+        
+        if(items.Length <= spawnLocations.Length)
+		{
+            SpawnItems();
+		}
+		else
+		{
+            Debug.Log("Za duzo itemów za mało spawnów");
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnItems()
+	{
+        List<int> listOfRandSpawns = new List<int>();
+        int spawn;
+        for(int i=0; i < items.Length; i++)
+		{
+            do
+            {
+                spawn = Random.Range(0, spawnLocations.Length);
+
+            } while (listOfRandSpawns.Contains(spawn));
+            listOfRandSpawns.Add(spawn);
+        }
+
+        int x = 0;
+        foreach (GameObject item in items)
+        {
+            
+            GameObject.Instantiate(item, spawnLocations[listOfRandSpawns[x]].transform.position, Quaternion.identity);
+            x++;
+        }
+
+    }
+}
